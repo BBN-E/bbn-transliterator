17c8893078be6e5e127e7cad7fb33872c0c22f73
Noah Rivkin <nrivkin@bbn.com>
2017-06-22 14:11:48 -0400

    Added missing  @Overrides

--- transliteration-core/src/main/java/com/bbn/serif/transliteration/Script.java
+++ transliteration-core/src/main/java/com/bbn/serif/transliteration/Script.java
@@ -291,6 +291,7 @@ abstract class Script {
       return data().get(offset.asInt());
     }
 
+    @Override
     public final boolean startsNewScript(CharOffset offset) {
       return offset.asInt() == 0 || Sets
           .intersection(data().get(offset.asInt()), data().get(offset.shiftedCopy(-1).asInt()))
@@ -327,6 +328,7 @@ abstract class Script {
       }
     }
 
+    @Override
     public boolean isAbugidaVowelForSomeScript(CharOffset offset, String vowel) {
       final String lowercaseVowel = vowel.toLowerCase(Locale.ENGLISH);
       for (final Script script : scriptsForOffset(offset)) {
