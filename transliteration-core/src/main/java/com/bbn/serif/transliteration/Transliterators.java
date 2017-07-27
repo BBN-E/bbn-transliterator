74bb3042608068826e195eac5a4a5c3130e2c9a1
Noah Rivkin <nrivkin@bbn.com>
2017-07-27 17:16:42 -0400

    Update Transliterators

--- transliteration-core/src/main/java/com/bbn/serif/transliteration/Transliterators.java
+++ transliteration-core/src/main/java/com/bbn/serif/transliteration/Transliterators.java
@@ -3,10 +3,16 @@ package com.bbn.serif.transliteration;
 import com.bbn.bue.common.StringNormalizer;
 import com.bbn.bue.common.StringUtils;
 import com.bbn.bue.common.TextGroupImmutable;
+import com.bbn.bue.common.parameters.Parameters;
+import com.bbn.bue.common.parameters.ParametersModule;
 
 import com.google.common.collect.SetMultimap;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
 
 import org.immutables.value.Value;
+import org.slf4j.LoggerFactory;
 
 import java.util.Set;
 
@@ -17,6 +23,18 @@ import static com.google.common.collect.Iterables.getOnlyElement;
  */
 @Value.Enclosing
 public final class Transliterators {
+
+  private static final org.slf4j.Logger log = LoggerFactory.getLogger(Transliterators.class);
+  private static Transliterator generalTransliterator;
+
+
+  private static void setupGeneralTransliterator() {
+    final Parameters emptyParams = Parameters.builder().build();
+    final Injector injector = Guice.createInjector(ParametersModule.createSilently(emptyParams),
+        new DefaultTransliterator.FromParamsModule(emptyParams));
+    generalTransliterator = injector.getInstance(Key.get(Transliterator.class,
+        DefaultTransliterator.GeneralTransliterator.class));
+  }
 
   private Transliterators() {
     throw new UnsupportedOperationException();
@@ -57,9 +75,15 @@ public final class Transliterators {
     if (registeredTransliterators.size() == 1) {
       return getOnlyElement(registeredTransliterators);
     } else if (registeredTransliterators.isEmpty()) {
-      throw new RuntimeException("No transliterators registered for "
-          + iso6392LanguageCode + ". Transliterators are registered for "
-          + iso6392ToLanguageMap.keySet());
+      // we want to return a general transliterator instead of throwing an error
+      log.warn("No transliterators registered for {}. Transliterators are registered for {}. Using "
+              + "general transliterator instead.", iso6392LanguageCode,
+              iso6392ToLanguageMap.keySet());
+      setupGeneralTransliterator();
+      return generalTransliterator;
+//      throw new RuntimeException("No transliterators registered for "
+//          + iso6392LanguageCode + ". Transliterators are registered for "
+//          + iso6392ToLanguageMap.keySet());
     } else {
       throw new RuntimeException("Multiple transliterators are registered for "
           + iso6392LanguageCode + ". You will need to use a custom module to "
